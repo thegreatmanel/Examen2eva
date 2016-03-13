039d27d243723c2d1929ab4f909c6783520f79df
Isaac <Isaac@192.168.1.130>
2016-03-13 21:43:02 +0100

    Usuario.

---
+++ src/miapp/Usuario.java
@@ -0,0 +1,40 @@
+package miapp;
+
+/**
+ * En esta clase está el usuario y su metodos de edición.
+ * @author Isaac
+ * @version 2.0
+ */
+public class Usuario {
+
+    private String usuario;
+
+    private String contrasenha;
+
+    private Libreta libreta;
+
+    /**
+     * Constructor por defecto.
+     */
+    public Usuario() {
+    }
+
+    /**
+     * Añade una libre de contactos,si hay un huevo libre.
+     */
+    public void engadirLibreta() {
+
+    }
+
+    /**
+     * La libreta de contactos resulta estar vacía.
+     */
+    public void borrarLibreta() {
+    }
+
+    /**
+     * Edita la libre de contactos,modifica los grupos y los contactos.
+     */
+    public void editarLibreta() {
+    }
+}
